0865904c315395c4b04f3b46d5449a19133c05ee
WhosAlbercik <whosalbercik@gmail.com>
2023-06-08 22:34:15 +0200

    Initial Commit

---
+++ src/main/java/whosalbercik/cashccexchange/CashccExchange.java
@@ -0,0 +1,18 @@
+package whosalbercik.cashccexchange;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(CashccExchange.MODID)
+public class CashccExchange {
+
+    // Define mod id
+    public static final String MODID = "cashccexchange";
+    // reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public CashccExchange() {
+        
+    }
+}
